19c28af2df5bbd3e4994e640eaafd852cd6c3c68
michiru6828 <since_l999@yahoo.co.jp>
2023-07-03 09:14:52 -0700

    added logout button

--- frontend/src/components/common/UI/Header/index.tsx
+++ frontend/src/components/common/UI/Header/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from '../Button'
 
 function Header() {
+  const handleLogoutButton = () => {
+    localStorage.removeItem('token')
+  }
   return (
     <>
       <header>
         <Link to={'/home'} className="headerHomeButton">
           <h3>
-            <img src="../../../../../../meetingIcon.png" className="icon"></img>
+            <img
+              src="../../../../../../meetingIcon.png"
+              alt="headerIcon"
+              className="icon"
+            ></img>
             人事管理 HR app
           </h3>
+          <Button
+            text={'ログアウト'}
+            onClick={handleLogoutButton}
+            className="logoutButton"
+          />
         </Link>
       </header>
     </>
